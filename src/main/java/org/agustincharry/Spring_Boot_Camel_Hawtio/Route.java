a0b295033b4f22d1a606981c77ba9037c10fb68c
Agustin Charry <hacharry@bancolombia.com.co>
2020-04-30 11:43:40 -0500

    Se añade dependencia camel management

--- src/main/java/org/agustincharry/Spring_Boot_Camel_Hawtio/Route.java
+++ src/main/java/org/agustincharry/Spring_Boot_Camel_Hawtio/Route.java
@@ -9,9 +9,8 @@ public class Route extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        //from("timer:aa?period=500")
         from("direct:aa")
-                .log("Hola Mundo------1");
+                .log("Hola Mundo------------------>>1");
 
 
         from("timer:aa?period=500")
